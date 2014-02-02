76fca8e52fbecd4a5a89e611af339ff181da532f
Ben Radler <ben@benradler.com>
2014-02-01 21:37:29 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BootstrapSassTest::Application.config.secret_key_base = '506d0c648806e1a61ebd82fc14b2ad3f3557714b15ccfdb7862bfdab8a2e1950de837e4bbb7b07a52ea9b04e3d4574f719b7a26170a78b59775da3cc2ccf4aa5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BootstrapSassTest::Application.config.session_store :cookie_store, key: '_bootstrap-sass-test_session'
